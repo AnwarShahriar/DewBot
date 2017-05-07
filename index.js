7b1b1fba73b4192915cc7c65d4f4471ca1119fa8
anwarshahriar <anwarshahriar@gmail.com>
2017-05-07 13:59:27 +0600

    Fetch follower list

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ var bot = new Twit({
     access_token_secret: process.env.DEWBOT_ACCESS_TOKEN_SECRET,
     timeout_ms: 60*1000 // 1 minute timeout
 })
+
+bot.get('followers/ids', {screen_name: 'anwarshahriar'}, function (err, data, response) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
